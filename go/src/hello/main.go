68271eef927a96914b2569138896b1d9bbec58ba
Rob Muhlestein <rob@skilstak.com>
2015-11-06 22:52:58 -0500

    save

--- go/src/hello/main.go
+++ go/src/hello/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	c "github.com/skilstak/go/colors"
 	"os"
+	"strings"
+	"time"
 )
 
 func plain(message string) {
@@ -16,13 +18,41 @@ func color(message string) {
 	}
 }
 
+func multi(message string) {
+	for {
+		fmt.Print(c.Clear + c.Rc() + message + " " + c.X)
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+
 func main() {
-	if len(os.Args) > 1 {
-		if os.Args[1] == "color" {
-			color("Hello World!")
+	var who = "world"
+	var option = ""
+	var message = ""
+
+	if len(os.Args) > 2 {
+		option = os.Args[1]
+		who = os.Args[2]
+
+	} else if len(os.Args) == 2 {
+		if strings.HasPrefix(os.Args[1], "-") {
+			option = os.Args[1]
+		} else {
+			who = os.Args[1]
 		}
+	}
+
+	message = "Hello " + who + "!"
+
+	// could do this more directly with switch or an arg
+	// parser, but keeping is simple for now
+
+	if option == "-m" {
+		multi(message)
+	} else if option == "-c" {
+		color(message)
 	} else {
-		plain("Hello World!")
+		plain(message)
 	}
 
 }
